53f0c920cc462b4bf6339ec64a7f4af057e83332
Bart Orbons <bart@bertrick.nl>
2022-04-19 12:43:26 +0000

    added second tag

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "azurerm_virtual_machine" "catapp" {
 
   tags = { 
     Department = "devops" 
-    Billable = True
+    Billable = true
   }
 
   # Added to allow destroy to work correctly.
